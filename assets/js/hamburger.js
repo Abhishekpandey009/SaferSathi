965ca70fd4909882348ba512fe15bb11dbed70bc
Abhishek Pandey <124862208+Abhishekpandey009@users.noreply.github.com>
2025-05-23 17:09:32 +0530

    Delete assets directory

--- assets/js/hamburger.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const navButtons = document.querySelector('.nav-buttons');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-  navButtons.classList.toggle('active');
-});
